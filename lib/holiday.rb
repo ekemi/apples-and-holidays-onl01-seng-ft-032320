3aa3672000ff0861b8f8c91ae0b42ccbd6ab7a5d
edmundo papi <edmundopierre@yahoo.com>
2020-04-02 12:40:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
     holiday_hash[:winter].each do |key, value|
-       value << "suppy"
+       value << "Balloons"
        end
     # end
 end
